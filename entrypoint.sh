c98103b52db244e90606d1f55f243c0ce8f1a4e4
Will <rut.willems@gmail.com>
2022-12-05 10:03:06 +0100

    remove file check and add no-check

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,39 +8,29 @@
 # Revert fetch with depth=1, so eveything is collpased to one single commit
 git pull --unshallow
 
-# Detecting changes files from latest commit
-echo "> Detecting changes files for commit $GITHUB_SHA..."
-git_diff=$(git show --name-only --oneline "${GITHUB_SHA}" | tail -n +2 | grep -E '\.(pipe|datasource)')
-
-# Print changed files
-echo "> Detected files:"
-for file in $git_diff; do
-  echo "$file"
-done
 
 FORCE=""
+NOCHECK=""
 POPULATE=""
 
-# Push changes
-for file in $git_diff; do
-  if [ ! -f "$file" ]; then
-    # Skip if detected file doesn't exist
-    continue
-  fi
 
-  echo "> Pushing $file..."
+# Force flag
+if echo $INPUT_FORCE | grep -iqF true; then
+  FORCE="--force"
+fi
 
-  # Force flag
-  if echo $INPUT_FORCE | grep -iqF true; then
-    FORCE="--force"
-  fi
+# No check
+if echo $INPUT_NOCHECK | grep -iqF true; then
+  NOCHECK="--no-check"
+fi
 
-  # Populate flag
-  if echo $INPUT_POPULATE | grep -iqF true; then
-    POPULATE="--populate"
-  fi
+# Populate flag
+if echo $INPUT_POPULATE | grep -iqF true; then
+  POPULATE="--populate"
+fi
 
-  # Print command
-  echo "tb --token ${TOKEN} --host "${HOST}" push --push-deps ${FORCE} $file ${POPULATE}"
-  tb --token "${TOKEN}" --host "${HOST}" push --push-deps ${FORCE} "$file" ${POPULATE}
-done
+
+# Print command
+echo "tb --token ${TOKEN} --host "${HOST}" push --push-deps ${FORCE} $file ${POPULATE}"
+tb --token "${TOKEN}" --host "${HOST}" push --push-deps ${FORCE} ${NOCHECK} "$file" ${POPULATE}
+
